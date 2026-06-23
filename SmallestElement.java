fb99cdc34821a1b0789245f58a5469f35805d564
lochan patidar <lochanpatidar6@gmail.com>
2026-06-23 08:58:51 -0700

    Create SmallestElement.java

---
+++ SmallestElement.java
@@ -0,0 +1,15 @@
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 20, 80, 30};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest = " + min);
+    }
+}
