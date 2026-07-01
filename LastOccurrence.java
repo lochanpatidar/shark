3965612cf439a2007ce7152787ba317e4f2bc661
lochan patidar <lochanpatidar6@gmail.com>
2026-07-01 09:58:49 -0700

    Create LastOccurrence.java

---
+++ LastOccurrence.java
@@ -0,0 +1,16 @@
+public class LastOccurrence {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 20, 20, 30, 40};
+        int key = 20;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                index = i;
+            }
+        }
+
+        System.out.println("Last Occurrence: " + index);
+    }
+}
